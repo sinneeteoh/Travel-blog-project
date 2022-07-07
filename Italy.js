eb97dd0e5362551b9d6e8c6ef8292d562e5e747f
sinneeteoh <sinneeteoh@gmail.com>
2022-07-07 18:25:57 +0100

    first commit

---
+++ Italy.js
@@ -0,0 +1,10 @@
+let Italy = prompt("Are you looking for our Italy travel page?");
+if(Italy === "yes"){
+    alert("You're in the right place! We hope you find the information you need :)");
+}
+else if(Italy === "no"){
+    alert("Try using our navbar to navigate to our alternate destination pages :)");
+}
+else{
+    alert("Sorry I didn't catch that, try answering yes or no :)");
+}
